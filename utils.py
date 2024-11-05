d3b7eddd58f44e70cbb928fe48f726f60444e8d3
Ravitejaa45 <115796612+Ravitejaa45@users.noreply.github.com>
2024-11-05 20:03:43 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,11 @@
 import re
 
-def clean_and_truncate_text(text, max_words=1500):  # Increased max_words for better context
-    # Current cleaning functions remain the same
+def clean_and_truncate_text(text, max_words=1500):
     # Remove HTML tags
     text = re.sub(r'<[^>]*?>', '', text)
     # Remove URLs
     text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
-    # Keep some special characters that might be relevant for job descriptions
+    # Remove special characters
     text = re.sub(r'[^a-zA-Z0-9\s\.\,\-\+\#\/\(\)\%]', '', text)
     # Replace multiple spaces with a single space
     text = re.sub(r'\s{2,}', ' ', text)
@@ -14,7 +13,6 @@ def clean_and_truncate_text(text, max_words=1500):  # Increased max_words for be
     text = text.strip()
     # Remove extra whitespace
     text = ' '.join(text.split())
-
     # Truncate to max_words
     words = text.split()
     if len(words) > max_words:
